4ce8615b628cba864e5f53d796f65783f5da8ca8
Erik <erik_cool_97@live.se>
2018-06-20 08:41:36 +0200

    functions for continue at work

--- server/login.js
+++ server/login.js
@@ -7,4 +7,20 @@ const sqlite3 = require('sqlite3').verbose();
 
 exports.loginAttempt = function(user, pass){
     console.log("User: "+ sha256(user));
+
+    let db = new sqlite3.Database(settings.dbLocation);
+    var date = getCurrentDate();
+    let sql = "SELECT * FROM Orders WHERE time_of_order <= datetime('"+date+"', '-600 seconds') AND is_placed = 0 ORDER BY id";
+
+    //Calling Database to get all orders that have waited more then 5min and is not placed yet
+    db.all(sql, [], (err,rows) => {
+        if(err) {
+            console.log("Error: " + err);
+        }
+        else{
+            rows.forEach((row) =>{
+
+
+
+
 }
