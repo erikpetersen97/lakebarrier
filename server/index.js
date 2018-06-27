a2d341d38f20b2a54c49c190106bd02694000179
Erik <erik_cool_97@live.se>
2018-06-27 09:47:42 +0200

    added error fixes and download of client

--- server/index.js
+++ server/index.js
@@ -6,9 +6,12 @@ const settings = require("./settings.json");
 var cors = require('cors')
 var bodyParser = require("body-parser");
 var sha256 = require("sha256");
+var mime = require("mime");
+
+
 
 const login = require("./login");
-
+const sqlite3 = require('sqlite3').verbose();
 //Creating Express App
 var app = express();
 
@@ -38,20 +41,99 @@ app.use(express.static(path.join(__dirname)));
 
 
 var htmlCodeLogin = "";
+
 fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err;
     }
     htmlCodeLogin = html;
 });
+var htmlCodeLoginError = "";
+fs.readFile('./indexError.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    htmlCodeLoginError = html;
+});
+
+
 
 //routes
 app.get('/', (req, res) => res.send(htmlCodeLogin));
 app.post("/", function(req,res){
-    console.log(req.body);
-    login.loginAttempt(req.body.username, req.body.password);
+
+    login.loginAttempt(req.body.username, req.body.password, req, res, processCallback);
+
 
 });
+function processCallback(value, req, res){
+
+    if(value){
+        console.log("Hipp Hurray Here is link");
+
+        var file = __dirname + '/test.txt';
+
+        var filename = path.basename(file);
+
+        res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+
+
+        var filestream = fs.createReadStream(file);
+        filestream.pipe(res);
+
+
+
+    }
+    else{
+        console.log("Sorry no Software for you");
+        res.send(htmlCodeLoginError);
+    }
+}
+
+
+//
+//
+// let db = new sqlite3.Database(settings.dbLocation);
+// var loginData = 'INSERT INTO LoginData(username, password)VALUES("erik", "pass");';
+//
+// var userData = 'INSERT INTO UserData(FirstName, LastName, Age, Country, Address, Zipcode, Email, ExpireDate, Permission)VALUES ('
+// + '"Erik", "Petersen", "20", "Sweden", "Nygatan 4", "28931", "erik_cool_97@live.se", "Tomorrow", "5"' +');';
+//
+//
+//
+//                         db.run(loginData, function(err){
+//                             if(err){
+//                                 console.log("First Error in db" + err);
+//                             }
+//                             else{
+//                                 console.log("Success insert LoginData");
+//                                 db.run(userData, function(err){
+//                                     if(err){
+//                                         console.log("Error in db" + err);
+//                                     }
+//                                     else{
+//                                         console.log("Success insert UserData");
+//
+//                                     }
+//                                 });
+//                             }
+//                         });
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
 
 
 
--- server/login.js
+++ server/login.js
@@ -5,22 +5,37 @@ var sha256 = require("sha256");
 
 const sqlite3 = require('sqlite3').verbose();
 
-exports.loginAttempt = function(user, pass){
-    console.log("User: "+ sha256(user));
-
-    let db = new sqlite3.Database(settings.dbLocation);
-    var date = getCurrentDate();
-    let sql = "SELECT * FROM Orders WHERE time_of_order <= datetime('"+date+"', '-600 seconds') AND is_placed = 0 ORDER BY id";
-
-    //Calling Database to get all orders that have waited more then 5min and is not placed yet
-    db.all(sql, [], (err,rows) => {
-        if(err) {
-            console.log("Error: " + err);
-        }
-        else{
-            rows.forEach((row) =>{
+exports.loginAttempt = function(user, pass, req, res, callback){
 
 
+    var db = new sqlite3.Database(settings.dbLocation);
+    var sql = "SELECT password FROM LoginData WHERE username = '"+user+"'";
+
+    //Calling Database to get user
+     db.all(sql, [], (err,rows) => {
+         if(err) {
+             console.log("Error: " + err);
+         }
+         else{
+             if(rows.length > 0){
+                 rows.forEach((row) =>{
+                     if(row.password === sha256(pass)){
+
+                         callback(true, req, res);
+                     }
+                     else{
+
+                         callback(false, req, res);
+                     }
+                 });
+             }
+             else{
+                 callback(false, req, res);
+             }
+
+         }
 
 
-}
+
+});
+};
